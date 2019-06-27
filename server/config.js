15ce47b8b21883008f69fc8aeb9b1ded6bdc7c61
Mariusz Mycek <m.mycek83@gmail.com>
2019-06-27 18:31:39 +0200

    Change database to MongoAtlas

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 const config = {
-  mongoURL: 'mongodb://localhost:27017/next-kanban',
+  mongoURL: 'mongodb+srv://dbUser:I0KTCZ8rl1HVUpW7@cluster0-m0yg4.mongodb.net/test?retryWrites=true&w=majority',
+  
   port: process.env.PORT || 3000,
 };
 
