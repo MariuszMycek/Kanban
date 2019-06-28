c72d873cd31beb9aa81e6d4deda63728da62c3eb
Mariusz Mycek <m.mycek83@gmail.com>
2019-06-28 13:12:57 +0200

    Change some component names

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Nav from '../Client/components/Nav';
-import Head from '../Client/components/Head';
+import Nav from '../Client/components/nav.component';
+import Head from '../Client/components/head.component';
 import Kanban from '../Client/components/Kanban/Kanban';
 
 const Home = () => {
